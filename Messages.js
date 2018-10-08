b0fc0bc132e8e28c22c4256946465691a2c49c76
Kenneth Dick <kenneth_dick@valpak.com>
2018-10-08 10:22:53 -0400

    Create Messages.js

---
+++ Messages.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const WELCOME = 'Local expert. What can we help you find?';
+
+const HELP = 'With Coupons by Valpak you can find deals from every kind of business, all looking for new customers like you.';
+
+const CONNECTERROR = 'There is a problem connecting to Wikipedia at this time. Please try again later.';
+
+const GODEEPER = 'More Detail?';
+
+const CARDTITLE = 'More deals';
+
+const MOREREPROMPTTEXT = 'Do you want more?';
+
+const NOMORE = 'There are no more for this search. Try another date by saying, find a paddleboarding deal.';
+
+const HELPREPROMPTTEXT = 'What type of business do you want?';
+
+const DIRECTIVESERVICEMESSAGE = 'Please wait...';
+
+const DIRECTIVEERRORMESSAGE = 'Cannot enqueue a progressive direcitve.';
+
+const GETEVENTSERRORMESSAGE = 'Cannot get deals right now, thanks for using the local coupons skill';
+
+const GOODBYE = 'Bye! Thanks for using Valpak local coupons';
+
+const UNHANDLED = 'This skill doesn\'t support that. Please ask something else.';
+
+
+module.exports = {
+    'WELCOME': WELCOME,
+    'HELP': HELP,
+    'CONNECTERROR': CONNECTERROR,
+    'GODEEPER': GODEEPER,
+    'CARDTITLE': CARDTITLE,
+    'MOREREPROMPTTEXT': MOREREPROMPTTEXT,
+    'NOMORE': NOMORE,
+    'HELPREPROMPTTEXT': HELPREPROMPTTEXT,
+    'DIRECTIVESERVICEMESSAGE': DIRECTIVESERVICEMESSAGE,
+    'DIRECTIVEERRORMESSAGE': DIRECTIVEERRORMESSAGE,
+    'GETEVENTSERRORMESSAGE' : GETEVENTSERRORMESSAGE,
+    'GOODBYE': GOODBYE,
+    'UNHANDLED': UNHANDLED
+};
