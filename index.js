71fe890e136bb1484abf22f7603ecaee85b63576
Kenneth Dick <kenneth_dick@valpak.com>
2018-10-08 09:52:43 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+console.log('Loading function');
+
+exports.handler = (event, context, callback) => {
+    var o = {} // empty Object
+    var key = 'Orientation Sensor';
+    o[key] = []; // empty Array, which you can push() values into
+    var data = {
+        name: 'CiCis Pizza',
+        phone: '555-555-5555',
+        address: '123 Main St, St. Petersburg FL 33701'
+    };
+    var data2 = {
+        name: 'ABC Pizza',
+        phone: '555-555-5555',
+        address: '123 Central Ave, St. Petersburg FL 33701'
+    };
+    o[key].push(data);
+    o[key].push(data2);
+    
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+    console.log('value1 =', event.key1);
+    callback(null, JSON.stringify(o));  // Echo back the first key value
+    //callback('Something went wrong');
+};
